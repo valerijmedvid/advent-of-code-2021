68584183ab5f9ab1fa2dbd2dabb2c8b9c49b30b9
Valerij Medviď <vmedvid@gmail.com>
2021-12-02 15:21:26 +0100

    add day1-part2

---
+++ day1/part2/main.py
@@ -0,0 +1,17 @@
+with open("input_data.txt") as f:
+    contents = f.readlines()
+numbers = list(map(lambda x: int(x.strip()), contents))
+
+previous = None
+counter = 0
+
+for index in range(len(numbers)):
+    current = 0
+    for num in numbers[index : index + 3]:
+        current += num
+
+    if previous and previous < current:
+        counter += 1
+    previous = current
+
+print(counter)
